06b0e3a9246c9edaeca5f8f766b7df5fa214754f
Sagisak <leonardo.tyoes@gmail.com>
2025-06-02 17:15:01 +0700

    adding making table code

---
+++ Query_sql/ChurnRate/ChurnTableMake_v1.sql
@@ -0,0 +1,15 @@
+/* 
+USE GORPWHBI;
+GO
+
+
+CREATE TABLE dbo.Cohort (
+     StoreId INT,
+    StoreName NVARCHAR(100),
+    Tahun_Bulan CHAR(6),  -- format: YYYYMM
+    Jumlah_Memb_Aktif_Awal_Periode INT,
+    Jumlah_Memb_Aktif_Akhir_Periode INT,
+    jumlah_member_baru INT,
+    jumlah_member_Berhenti INT 
+);
+*/
---
+++ Query_sql/CohortSQL/CohortMakeTable_v1.sql
@@ -0,0 +1,11 @@
+/* 
+USE GORPWHBI;
+GO
+
+
+CREATE TABLE dbo.Cohort (
+     cohort_label NVARCHAR(50),
+    month_offset INT,
+    active_users INT
+);
+*/
